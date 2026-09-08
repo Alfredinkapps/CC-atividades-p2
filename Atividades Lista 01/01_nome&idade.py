9d1f895a315b9f933e106fb9fac64bff1016bb5c
Alfredo Alves Moreira Neto <4lfr3d0182@gmail.com>
2026-09-08 11:45:52 -0300

    Adicionando atividades e exercicios

---
+++ Atividades Lista 01/01_nome&idade.py
@@ -0,0 +1,3 @@
+nome = input("Qual seu nome?")
+idade = input("Qual sua idade?")
+resulta = print(f"Olá {nome}! Voce tem {idade} anos") 
---
+++ Atividades Lista 01/04_dobro.py
@@ -0,0 +1,3 @@
+num = int(input("digite um numero: "))
+mult = num*2
+print(f"o dobro do seu numero é {mult}!")
