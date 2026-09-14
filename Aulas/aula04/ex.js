6b1ef7cf05c3b6b7a9fab0f85c25acf14010a696
alfredo <4lfr3d0182@gmail.com>
2026-09-14 20:18:37 -0300

    JavaScript

---
+++ Aulas/aula04/ex.js
@@ -0,0 +1,9 @@
+let total = 10
+total += 5;
+total -= 1;
+
+let contador = 0;
+contador++;
+
+console.log(total);
+console.log(contador);
---
+++ Aulas/aula04/ex1.js
@@ -0,0 +1,6 @@
+const nome = "Ana";
+const nota1 = 7;
+const nota2 = 9;
+
+const media = (nota1 + nota2) / 2;
+ console.log(`A média de ${nome} é ${media}`);
